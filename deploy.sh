eb20f1cd683e99cada7a1fd6a06ed5a678265b6f
yanen Chen <sh5ojkmt@gmail.com>
2020-06-17 14:19:21 +0800

    檔案修正大小寫

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:yanennnnn/helthofDayCli3.git master:gh-pages
+
+cd -
